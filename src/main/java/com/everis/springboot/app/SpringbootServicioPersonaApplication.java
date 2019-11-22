b629ccdd4477d4a1a38cc85c17033d47c7cedce5
rimegaray <rimegaraysanmarcos@gmail.com>
2019-11-22 16:40:55 -0500

    Agregando conexion con Eureka

--- src/main/java/com/everis/springboot/app/SpringbootServicioPersonaApplication.java
+++ src/main/java/com/everis/springboot/app/SpringbootServicioPersonaApplication.java
@@ -12,10 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 
 import reactor.core.publisher.Flux;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringbootServicioPersonaApplication implements CommandLineRunner {
 
